447282bbe6ee374d8ba772b1ad757d8ae7558ec5
Z1618 <18779063880@163.com>
2022-10-27 17:21:21 +0800

    用户列表功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,140 @@
 <template>
-    <div>
-        <el-button type="info" @click="out">退出</el-button>
-    </div>
+        
+    <el-container class="homeContainer">
+        <!--头部-->
+        <el-header>
+            <div>
+                <img src="../assets/tubiao.png" alt="">
+                <span>电商管理平台</span>
+            </div>
+            <el-button type="info" @click="out">退出</el-button>
+        </el-header>
+        <!-- 页面主体-->
+        <el-container>
+            <!-- 主体侧边栏-->
+            <el-aside :width="isCollapse?'64px':'200px'">
+                <!--展开按钮-->
+                <div class="toggle" @click="toggleCollapse">|||</div>
+                <!--侧边菜单栏-->
+                <el-menu  background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" 
+                :collapse-transition="false" router :default-active="this.activePath" >
+                    <!--一级菜单-->
+                    <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
+                        <!--一级模板区域-->
+                        <template slot="title">
+                            <i :class="iconarr[item.id]"></i><!--图标-->
+                            <span>{{item.authName}}</span>
+                        </template>
+                        <!--二级模板区域---->
+                        <el-menu-item :index="subitem.path" v-for="subitem in item.children" 
+                        :key="subitem.id" @click="savenavState(subitem.path)">
+                            <template slot="title"> 
+                                <i class="el-icon-menu"></i><!--图标-->
+                                <span>{{subitem.authName}}</span>
+                            </template>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <!--右侧主体-->
+            <el-main>
+                <!--路由占位符-->
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
-import router from '@/router';
 export default {
+    data(){
+       return{
+         //左侧菜单栏
+         menulist:[],
+         iconarr:{
+            125:'iconfont icon-users',
+            103:'iconfont icon-tijikongjian',
+            101:'iconfont icon-shangpin',
+            102:'iconfont icon-danju',
+            145:'iconfont icon-baobiao'
+         },
+         isCollapse:false,
+         avtivePath:''
+       }
+    },
+    created(){
+        this.getMenuList()
+        this.activePath= window.sessionStorage.getItem('activePath')
+        console.log(this.activePath)
+    },
     methods:{
         out(){
             window.sessionStorage.clear();
             this.$router.push('/login')
+        },
+        //获取菜单栏
+        async getMenuList(){
+         const {data:res}= await  this.$http.get('menus')
+         if(res.meta.status !==200) return this.$message.msg
+         this.menulist=res.data
+         console.log(res)
+        },
+        //侧边栏展开事件
+        toggleCollapse(){
+            this.isCollapse=!this.isCollapse
+        },
+        //保存链接的激活状态
+        savenavState(activePath){
+            window.sessionStorage.setItem('activePath',activePath)
+            this.activePath=activePath
+            
         }
     }
 }
 </script>
 
 <style lang="less" scoped>
-
+.el-header{
+    background-color: #171d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+        display: flex;
+        align-items: center;
+        span {
+            margin-left: 15px;
+        }
+        img {
+            height: 100%;
+        }
+    }
+}
+.el-aside{
+    background-color: #333744;
+    .el-menu{
+        border-right: none;
+    }
+    .toggle{
+        background-color: #4a5064;
+        line-height: 24px;
+        font-size: 12px;
+        text-align: center;
+        color: #fff;
+        letter-spacing: 0.2em;
+        cursor: pointer;
+    }
+}
+.el-main{
+    background-color: #eaedf1;
+}
+.homeContainer{
+    height: 100%;
+}
+.iconfont {
+    margin-right: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -11,8 +11,13 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 //配置根路径
 axios.defaults.baseURL=`http://127.0.0.1:8888/api/private/v1/`
+axios.interceptors.request.use (config => {
+  // 在发送请求之前保存token
+  console.log(config)
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  return config;
+})
 Vue.prototype.$http = axios
-
 Vue.config.productionTip = false
 
 new Vue({
